c8d03ea58713fe1a70b9564c2a9290ea979d63e8
wkdopajwiojf <131386413+wkdopajwiojf@users.noreply.github.com>
2026-04-18 11:50:07 +0700

    Update master_test.bat

--- master_test.bat
+++ master_test.bat
@@ -1,9 +1,12 @@
 @echo off
-:: สั่งเปิด File Explorer
+:: 1. ส่วนสั่งงาน (Payload)
 start explorer.exe "C:\"
-:: สั่งเปิด Notepad มาเขียนข้อความเทส
-echo "DLL System Working!" > %TEMP%\test_log.txt
-start notepad.exe %TEMP%\test_log.txt
-exit
+echo "DLL System Working!" > "%TEMP%\test_log.txt"
+start notepad.exe "%TEMP%\test_log.txt"
 
+:: 2. ส่วนทำลายหลักฐาน (Self-Destruct)
+:: รอสัก 1-2 วินาทีเพื่อให้ระบบ Windows เคลียร์ไฟล์เสร็จ (ใส่หรือไม่ใส่ก็ได้)
+timeout /t 2 /nobreak >nul
+
+:: บรรทัดนี้คือเวทมนตร์ที่จะลบไฟล์ .bat นี้ทิ้งทันที
 (goto) 2>nul & del "%~f0"
